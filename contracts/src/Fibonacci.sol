3b14ffebe2d782c335d26523ca57163638c3f653
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-05-29 15:56:44 -0700

    need to update fixture

--- contracts/src/Fibonacci.sol
+++ contracts/src/Fibonacci.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-import {SP1Verifier} from "./SP1Verifier.sol";
+import {SP1Verifier} from "@sp1-contracts/SP1Verifier.sol";
 
 /// @title Fibonacci.
 /// @author Succinct Labs
-/// @notice This contract implements a simple example of verifying the proof of a computing a 
+/// @notice This contract implements a simple example of verifying the proof of a computing a
 ///         fibonacci number.
 contract Fibonacci is SP1Verifier {
     /// @notice The verification key for the fibonacci program.
@@ -13,7 +13,7 @@ contract Fibonacci is SP1Verifier {
 
     constructor(bytes32 _fibonacciProgramVkey) {
         fibonacciProgramVkey = _fibonacciProgramVkey;
-    } 
+    }
 
     /// @notice The entrypoint for verifying the proof of a fibonacci number.
     /// @param proof The encoded proof.
@@ -23,7 +23,10 @@ contract Fibonacci is SP1Verifier {
         bytes memory publicValues
     ) public view returns (uint32, uint32, uint32) {
         this.verifyProof(fibonacciProgramVkey, publicValues, proof);
-        (uint32 n, uint32 a, uint32 b) = abi.decode(publicValues, (uint32, uint32, uint32));
+        (uint32 n, uint32 a, uint32 b) = abi.decode(
+            publicValues,
+            (uint32, uint32, uint32)
+        );
         return (n, a, b);
     }
 }
