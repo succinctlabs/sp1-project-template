92d857d77403a493b187c955c98f12226e325899
John Guibas <jtguibas@gmail.com>
2024-06-11 18:46:20 -0700

    feat: standard vs evm

--- README.md
+++ README.md
@@ -5,23 +5,35 @@ that can generate a proof of any RISC-V program and verify the proof onchain.
 
 ## Requirements
 
-- [Go](https://go.dev/doc/install)
 - [Rust](https://rustup.rs/)
 - [SP1](https://succinctlabs.github.io/sp1/getting-started/install.html)
 - [Foundry](https://book.getfoundry.sh/getting-started/installation)
 
 ## Generate Proof
 
-Generate the proof that will be used as a fixture in the contracts directory.
+**Standard Proof Generation.**
+
+Generate the proof for your program using the standard prover.
 
 ```
 cd script
 RUST_LOG=info cargo run --bin prove --release
 ```
 
+**EVM-Compatible Proof Generation.**
+
+*WARNING: You will need at least 128GB of RAM to generate the proof.*
+
+Generate the proof that is small enough to be verified on-chain and verifiable by the EVM. This command also generates a fixture that can be used to test the verification of SP1 zkVM proofs inside Solidity.
+
+```
+cd script
+RUST_LOG=info cargo run --bin prove --release -- --evm
+```
+
 ## Solidity Proof Verification
 
-Verify the proof with the SP1 EVM verifier.
+After generating the Verify the proof with the SP1 EVM verifier.
 
 ```
 cd ../contracts
--- script/src/bin/prove.rs
+++ script/src/bin/prove.rs
@@ -24,6 +24,9 @@ pub const FIBONACCI_ELF: &[u8] = include_bytes!("../../../program/elf/riscv32im-
 struct ProveArgs {
     #[clap(long, default_value = "500")]
     n: u32,
+
+    #[clap(long, default_value = "false")]
+    evm: bool,
 }
 
 /// The public values encoded as a tuple that can be easily deserialized inside Solidity.
@@ -60,47 +63,56 @@ fn main() {
     let mut stdin = SP1Stdin::new();
     stdin.write(&args.n);
 
-    // Generate the proof.
-    let proof = client
-        .prove_plonk(&pk, stdin)
-        .expect("failed to generate proof");
+    if args.evm {
+        // Generate the proof.
+        let proof = client
+            .prove_plonk(&pk, stdin)
+            .expect("failed to generate proof");
 
-    // Deserialize the public values.
-    let bytes = proof.public_values.as_slice();
-    let (n, a, b) = PublicValuesTuple::abi_decode(bytes, false).unwrap();
+        // Deserialize the public values.
+        let bytes = proof.public_values.as_slice();
+        let (n, a, b) = PublicValuesTuple::abi_decode(bytes, false).unwrap();
 
-    // Create the testing fixture so we can test things end-ot-end.
-    let fixture = SP1FibonacciProofFixture {
-        a,
-        b,
-        n,
-        vkey: vk.bytes32().to_string(),
-        public_values: proof.public_values.bytes().to_string(),
-        proof: proof.bytes().to_string(),
-    };
+        // Create the testing fixture so we can test things end-ot-end.
+        let fixture = SP1FibonacciProofFixture {
+            a,
+            b,
+            n,
+            vkey: vk.bytes32().to_string(),
+            public_values: proof.public_values.bytes().to_string(),
+            proof: proof.bytes().to_string(),
+        };
 
-    // The verification key is used to verify that the proof corresponds to the execution of the
-    // program on the given input.
-    //
-    // Note that the verification key stays the same regardless of the input.
-    println!("Verification Key: {}", fixture.vkey);
+        // The verification key is used to verify that the proof corresponds to the execution of the
+        // program on the given input.
+        //
+        // Note that the verification key stays the same regardless of the input.
+        println!("Verification Key: {}", fixture.vkey);
 
-    // The public values are the values whicha are publically commited to by the zkVM.
-    //
-    // If you need to expose the inputs or outputs of your program, you should commit them in
-    // the public values.
-    println!("Public Values: {}", fixture.public_values);
+        // The public values are the values whicha are publically commited to by the zkVM.
+        //
+        // If you need to expose the inputs or outputs of your program, you should commit them in
+        // the public values.
+        println!("Public Values: {}", fixture.public_values);
 
-    // The proof proves to the verifier that the program was executed with some inputs that led to
-    // the give public values.
-    println!("Proof Bytes: {}", fixture.proof);
+        // The proof proves to the verifier that the program was executed with some inputs that led to
+        // the give public values.
+        println!("Proof Bytes: {}", fixture.proof);
 
-    // Save the fixture to a file.
-    let fixture_path = PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../contracts/src/fixtures");
-    std::fs::create_dir_all(&fixture_path).expect("failed to create fixture path");
-    std::fs::write(
-        fixture_path.join("fixture.json"),
-        serde_json::to_string_pretty(&fixture).unwrap(),
-    )
-    .expect("failed to write fixture");
+        // Save the fixture to a file.
+        let fixture_path =
+            PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("../contracts/src/fixtures");
+        std::fs::create_dir_all(&fixture_path).expect("failed to create fixture path");
+        std::fs::write(
+            fixture_path.join("fixture.json"),
+            serde_json::to_string_pretty(&fixture).unwrap(),
+        )
+        .expect("failed to write fixture");
+    } else {
+        // Generate the proof.
+        let proof = client.prove(&pk, stdin).expect("failed to generate proof");
+
+        // Verify the proof.
+        client.verify(&proof, &vk).expect("failed to verify proof");
+    }
 }
