1cc66a5af5d19cd2dd58997fa208a7412cf928aa
Matt Stam <mattstam@live.com>
2024-07-02 11:34:09 -0700

    chore: update to sp1 dev (#14)

--- script/src/bin/prove.rs
+++ script/src/bin/prove.rs
@@ -11,7 +11,7 @@ use std::path::PathBuf;
 use alloy_sol_types::{sol, SolType};
 use clap::Parser;
 use serde::{Deserialize, Serialize};
-use sp1_sdk::{HashableKey, ProverClient, SP1PlonkBn254Proof, SP1Stdin, SP1VerifyingKey};
+use sp1_sdk::{HashableKey, PlonkBn254Proof, ProverClient, SP1Proof, SP1Stdin, SP1VerifyingKey};
 
 /// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
 ///
@@ -56,12 +56,21 @@ fn main() {
     if args.evm {
         // Generate the proof.
         let proof = client
-            .prove_plonk(&pk, stdin)
+            .prove(&pk, stdin)
+            .plonk()
+            .run()
             .expect("failed to generate proof");
-        create_plonk_fixture(&proof, &vk);
+        match &proof.proof {
+            SP1Proof::Plonk(plonk_proof) => create_plonk_fixture(plonk_proof, &vk),
+            _ => panic!("Error: Expected Plonk Proof, but found a different proof type."),
+        }
     } else {
         // Generate the proof.
-        let proof = client.prove(&pk, stdin).expect("failed to generate proof");
+        let proof = client
+            .prove(&pk, stdin)
+            .core()
+            .run()
+            .expect("failed to generate proof for");
         let (_, _, fib_n) =
             PublicValuesTuple::abi_decode(proof.public_values.as_slice(), false).unwrap();
         println!("Successfully generated proof!");
@@ -85,9 +94,10 @@ struct SP1FibonacciProofFixture {
 }
 
 /// Create a fixture for the given proof.
-fn create_plonk_fixture(proof: &SP1PlonkBn254Proof, vk: &SP1VerifyingKey) {
+fn create_plonk_fixture(proof: &PlonkBn254Proof, vk: &SP1VerifyingKey) {
     // Deserialize the public values.
-    let bytes = proof.public_values.as_slice();
+    let binding = proof.public_inputs.clone().concat();
+    let bytes = binding.as_bytes();
     let (n, a, b) = PublicValuesTuple::abi_decode(bytes, false).unwrap();
 
     // Create the testing fixture so we can test things end-ot-end.
@@ -96,15 +106,15 @@ fn create_plonk_fixture(proof: &SP1PlonkBn254Proof, vk: &SP1VerifyingKey) {
         b,
         n,
         vkey: vk.bytes32().to_string(),
-        public_values: proof.public_values.bytes().to_string(),
-        proof: proof.bytes().to_string(),
+        public_values: proof.public_inputs.concat().to_string(),
+        proof: proof.raw_proof.to_string(),
     };
 
     // The verification key is used to verify that the proof corresponds to the execution of the
     // program on the given input.
     //
     // Note that the verification key stays the same regardless of the input.
-    println!("Verification Key: {}", fixture.vkey);
+    println!("Program Verification Key: {}", fixture.vkey);
 
     // The public values are the values whicha are publically commited to by the zkVM.
     //
