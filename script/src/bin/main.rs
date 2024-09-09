a9aa4916a430f067d5b69c995495ec9e54e2e1d2
mattstam <codeguy0112358@gmail.com>
2024-09-09 19:24:04 +0000

    updates

--- script/src/bin/main.rs
+++ script/src/bin/main.rs
@@ -79,6 +79,7 @@ fn main() {
         // Generate the proof
         let proof = client
             .prove(&pk, stdin)
+            .compressed()
             .run()
             .expect("failed to generate proof");
 
